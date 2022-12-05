182abdcd00d523818134dfa93e02d1a0a3c3d026
DanielQCarvalho <dc.queijo01@gmail.com>
2022-12-05 23:04:38 +0000

    D5P1 Done, took a while :) Possibly a better way to parse input

--- src/main/kotlin/D5/D5P1.kt
+++ src/main/kotlin/D5/D5P1.kt
@@ -3,52 +3,57 @@ package D5
 import java.io.File
 
 fun main(){
-    val bufferedReader = File("src/main/kotlin/D5/input.txt").bufferedReader()
-    var stackLists = Array<MutableList<String>?>(1) { mutableListOf() }
+    val timeStart = System.currentTimeMillis()
+    val bufferedReader = File("src/main/kotlin/D5/aoc_2022_day05_large_input.txt").bufferedReader()
+    val stackLists = mutableListOf<MutableList<String>>(mutableListOf())
 
     bufferedReader.useLines { lines ->
         lines.forEach { l ->
             if(l.isNotEmpty()){
-                var stackData = l.split("\\s{1,4}".toRegex())
-                var stack = l.trim().split("\\s{1,3}".toRegex())
+                val stackData = l.split("\\s{1,4}".toRegex())
+                val stack = l.trim().split("\\s{1,3}".toRegex())
+                //println(stackData)
+                //println(stack)
                 if(stackData[0] != "move" && stack[0].toIntOrNull() == null){
                     stackData.forEachIndexed { index, s ->
                         if (s != "") {
-                            println("Letter $s Index $index")
+                            //println("Letter $s Index $index")
                             if (stackLists.getOrNull(index) == null) {
-                                stackLists = stackLists.copyOf(index+1)
-                                stackLists[index] = mutableListOf(s)
+                                repeat(index-stackLists.size+1) { stackLists.add(mutableListOf()) }
+                                //println(stackLists)
+                                stackLists[index].add(s)
+                                //println("Copied")
                             } else {
-                                stackLists[index]?.plusAssign(s)
+                                stackLists[index].add(s)
+                                //println("Added")
                             }
                         }
+                        //stackLists.forEach { println(" $it ") }
                     }
                 }else{
                     if(stackData.size == 6){
-                        println("================")
-                        print("Original ->")
-                        stackLists.forEach { print(" $it ") }
-                        println()
+                        //println("================")
+                        //print("Original ->")
+                        //stackLists.forEach { print(" $it ") }
+                        //println()
                         val source = stackLists[stackData[3].toInt()-1]
-                        println("From -> $source")
+                        //println("From -> $source")
                         val amount = stackData[1]
-                        println("Amount -> $amount")
-                        var end = stackLists[stackData[5].toInt()-1]
-                        println("To -> $end")
-                        if (end != null) {
-                            if (source != null) {
-                                //end += (source.take(amount.toInt())).reversed()
-                                end.addAll(0,source.take(amount.toInt()).reversed())
-                            }
-                        }
-                        stackLists[stackData[3].toInt()-1] = source?.drop(amount.toInt())?.toMutableList()
-                        println("Result ->")
-                        stackLists.forEach { print(" $it ") }
-                        println()
+                        //println("Amount -> $amount")
+                        val end = stackLists[stackData[5].toInt()-1]
+                        //println("To -> $end")
+                        end.addAll(0,source.take(amount.toInt()).reversed())
+                        stackLists[stackData[3].toInt()-1] = source.drop(amount.toInt()).toMutableList()
+                        //println("Result ->")
+                        //stackLists.forEach { print(" $it ") }
+                        //println()
                     }
                 }
             }
         }
     }
-    stackLists.forEach { println("Stack ${stackLists.indexOf(it)} has crate ${it?.firstOrNull()}. ") }
+    println()
+    stackLists.forEach { println("Stack ${stackLists.indexOf(it)+1} has crate ${it.firstOrNull()}. ") }
+    val timeEnd = System.currentTimeMillis()
+    println("Took ${(timeEnd-timeStart)/1000} seconds")
 }
