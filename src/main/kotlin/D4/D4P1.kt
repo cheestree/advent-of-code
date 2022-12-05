9ecafd26744966c453e0ac196e70344a0f6a90bc
DanielQCarvalho <dc.queijo01@gmail.com>
2022-12-05 01:22:34 +0000

    Day 4 Done :)

---
+++ src/main/kotlin/D4/D4P1.kt
@@ -0,0 +1,21 @@
+package D4
+
+import java.io.File
+
+fun main(){
+    val bufferedReader = File("src/main/kotlin/D4/input.txt").bufferedReader()
+    var count = 0
+
+    bufferedReader.useLines { lines ->
+        lines.forEach { l ->
+            val vals = l.split("-",",").map { it.toInt() }
+            val firstRange = ((vals[0]..vals[1]).toList())
+            val secondRange = ((vals[2]..vals[3]).toList())
+            //count += if(vals[0] <= vals[2] && vals[1] >= vals[3] || (vals[2] <= vals[0] && vals[3] >= vals[1] )) 1 else 0
+            count += if(firstRange.containsAll(secondRange) || (secondRange).containsAll(firstRange)) 1 else 0
+        }
+    }
+    println(count)
+}
+
+//433
