50c527709d93b64e2d0d373ab27660d6a9177b44
DanielQCarvalho <dc.queijo01@gmail.com>
2022-12-07 00:51:52 +0000

    D6 done :)

---
+++ src/main/kotlin/D6/D6P2.kt
@@ -0,0 +1,19 @@
+package D6
+
+import java.io.File
+
+fun main() {
+
+    val bufferedReader = File("src/main/kotlin/D6/input.txt").bufferedReader()
+    var count = 0
+
+    bufferedReader.useLines { lines ->
+        lines.forEach { l ->
+            val uniqueLine = l.toCharArray().toList()
+            for(i in 0..uniqueLine.size){
+                if(uniqueLine.subList(i,i+14).distinct().size == 14) { count = i+14; break }
+            }
+        }
+    }
+    println(count)
+}
